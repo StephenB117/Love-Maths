ffe1ec4f232251faa378baeea927457894a69e3f
Stephen Bateson <stephen.lambkin@gmail.com>
2024-04-17 09:59:58 +0000

    feat: added functions to increment score and wrong answer boxs

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,9 +50,11 @@ function checkAnswer() {
     
     if(isCorrect) {
         alert("Hey! you got it right");
+        incrementScore();
     }
     else {
         alert(`Awww.... you answered ${userAnswer}. The correct abswer was ${calculatedAnswer[0]}`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -80,11 +82,23 @@ function calculateCorrectAnswer () {
 
 }
 
+/**
+ * Gets the current score from the DOm and increments it by 1 
+ */
 function  incrementScore() {
     
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1 
+ */
 function  incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
     
 }
 
