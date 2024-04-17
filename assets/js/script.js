8cca6d311ec94bfff84e10178b86ad81bbd0f3ca
Stephen Bateson <stephen.lambkin@gmail.com>
2024-04-17 12:25:11 +0000

    feat: added division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -152,10 +152,10 @@ function  displayMultiplyQuestion(operand1, operand2) {
 
 }
 
-function  displayDivisionQuestion() {
+function  displayDivisionQuestion(operand1, operand2) {
 
-    document.getElementById("operand1").textContent = operand1 % operand2 == 0 ? operand1 : operand2;
-    document.getElementById("operand2").textContent = operand1 % operand2 == 0 ? operand2 : operand1;
+    document.getElementById("operand1").textContent = operand1 * operand2;
+    document.getElementById("operand2").textContent = operand1;
     document.getElementById("operator").textContent = "/";
 
 }
